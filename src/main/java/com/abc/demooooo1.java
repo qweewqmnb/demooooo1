1ec7cc08419f2584a6fcb4eb0b7652a6d056f09f
oo <uioertert@163.com>
2018-12-09 11:10:19 +0800

    1109

--- src/main/java/com/abc/demooooo1.java
+++ src/main/java/com/abc/demooooo1.java
@@ -4,5 +4,6 @@ public class demooooo1 {
     public static void main(String[] args) {
         System.out.println("111111111112222222222222222222000000000");
         System.out.println("qqqqqqqqqqqqqqqqqqqqqqqqqqq");
+        System.out.println("11111111111111");
     }
 }
--- src/main/java/com/abc/demoooooo2.java
+++ src/main/java/com/abc/demoooooo2.java
@@ -4,5 +4,6 @@ public class demoooooo2 {
     public static void main(String[] args) {
         System.out.println("00000000000000000000");
         System.out.println("wwwwwwwwwwwwww");
+
     }
 }
