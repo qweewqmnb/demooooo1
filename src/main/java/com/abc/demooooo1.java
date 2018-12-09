ebe347552bb848ccb5401a5213016198ffbe8e7c
oo <uioertert@163.com>
2018-12-09 10:57:17 +0800

    1057

--- src/main/java/com/abc/demooooo1.java
+++ src/main/java/com/abc/demooooo1.java
@@ -3,5 +3,6 @@ package com.abc;
 public class demooooo1 {
     public static void main(String[] args) {
         System.out.println("111111111112222222222222222222000000000");
+        System.out.println("qqqqqqqqqqqqqqqqqqqqqqqqqqq");
     }
 }
--- src/main/java/com/abc/demoooooo2.java
+++ src/main/java/com/abc/demoooooo2.java
@@ -3,5 +3,6 @@ package com.abc;
 public class demoooooo2 {
     public static void main(String[] args) {
         System.out.println("00000000000000000000");
+        System.out.println("wwwwwwwwwwwwww");
     }
 }
